a1c55f807a069dfb1564e782cffc56dced88d332
elliot_glb <elliot_glb0635@163.com>
2020-03-11 11:06:14 +0800

    consul注册中心的使用

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Elliot Ji
+ * @date 2020/3/11.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
